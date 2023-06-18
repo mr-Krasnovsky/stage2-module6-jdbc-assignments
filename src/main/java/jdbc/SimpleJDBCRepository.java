313c78b1bf2ef6764e14e8837f511305407623e3
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-06-18 10:32:31 +0300

    Rename some constants names

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -20,19 +20,19 @@ public class SimpleJDBCRepository {
     private PreparedStatement ps = null;
     private Statement st = null;
 
-    private static final String createUserSQL = "INSERT INTO MYUSERS (FIRSTNAME, LASTNAME, AGE) VALUES (?,?,?)";
-    private static final String updateUserSQL = "UPDATE MYUSERS SET FIRSTNAME=?, LASTNAME=?, AGE=? WHERE ID=?";
-    private static final String deleteUserSQL = "DELETE FROM MYUSERS WHERE ID=?";
-    private static final String findUserByIdSQL = "SELECT*FROM MYUSERS WHERE ID=?";
-    private static final String findUserByNameSQL = "SELECT*FROM MYUSERS WHERE FIRSTNAME=?";
-    private static final String findAllUserSQL = "SELECT*FROM MYUSERS";
+    private static final String CREATE_USER_SQL = "INSERT INTO MYUSERS (FIRSTNAME, LASTNAME, AGE) VALUES (?,?,?)";
+    private static final String UPDATE_USER_SQL = "UPDATE MYUSERS SET FIRSTNAME=?, LASTNAME=?, AGE=? WHERE ID=?";
+    private static final String DELETE_USER_SQL = "DELETE FROM MYUSERS WHERE ID=?";
+    private static final String FIND_USER_BY_ID_SQL = "SELECT*FROM MYUSERS WHERE ID=?";
+    private static final String FIND_USER_BY_NAME_SQL = "SELECT*FROM MYUSERS WHERE FIRSTNAME=?";
+    private static final String FIND_ALL_USER_SQL = "SELECT*FROM MYUSERS";
 
 
     public Long createUser(User user) {
         Long id = null;
         try {
             connection = CustomDataSource.getInstance().getConnection();
-            ps = connection.prepareStatement(createUserSQL, Statement.RETURN_GENERATED_KEYS);
+            ps = connection.prepareStatement(CREATE_USER_SQL, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, user.getFirstName());
             ps.setString(2, user.getLastName());
             ps.setInt(3, user.getAge());
@@ -53,7 +53,7 @@ public class SimpleJDBCRepository {
         User user = null;
         try {
             connection = CustomDataSource.getInstance().getConnection();
-            ps = connection.prepareStatement(findUserByIdSQL);
+            ps = connection.prepareStatement(FIND_USER_BY_ID_SQL);
             ps.setLong(1, userId);
             ResultSet resultSet = ps.executeQuery();
             if (resultSet.next()) {
@@ -71,7 +71,7 @@ public class SimpleJDBCRepository {
         User user = null;
         try {
             connection = CustomDataSource.getInstance().getConnection();
-            ps = connection.prepareStatement(findUserByNameSQL);
+            ps = connection.prepareStatement(FIND_USER_BY_NAME_SQL);
             ps.setString(1, userName);
             ResultSet resultSet = ps.executeQuery();
 
@@ -91,7 +91,7 @@ public class SimpleJDBCRepository {
         try {
             connection = CustomDataSource.getInstance().getConnection();
             st = connection.createStatement();
-            ResultSet resultSet = st.executeQuery(findAllUserSQL);
+            ResultSet resultSet = st.executeQuery(FIND_ALL_USER_SQL);
 
             while (resultSet.next()) {
                 Long id = resultSet.getLong("id");
@@ -113,7 +113,7 @@ public class SimpleJDBCRepository {
     public User updateUser(User user) {
         try {
             connection = CustomDataSource.getInstance().getConnection();
-            ps = connection.prepareStatement(updateUserSQL);
+            ps = connection.prepareStatement(UPDATE_USER_SQL);
             ps.setLong(4, user.getId());
             ps.setString(1, user.getFirstName());
             ps.setString(2, user.getLastName());
@@ -130,7 +130,7 @@ public class SimpleJDBCRepository {
     public void deleteUser(Long userId) {
         try {
             connection = CustomDataSource.getInstance().getConnection();
-            ps = connection.prepareStatement(deleteUserSQL);
+            ps = connection.prepareStatement(DELETE_USER_SQL);
             ps.setLong(1, userId);
             ps.executeUpdate();
         } catch (SQLException e) {
